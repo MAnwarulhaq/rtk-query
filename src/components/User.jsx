3a98d57ddfd2fabf0802d243a01c15b273aeada3
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-11-25 18:39:31 +0500

    update the code

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -22,6 +22,7 @@ const User = () => {
 
   return (
     <div className='my-10 px-4'>
+      <input type="text" name="" id="" onChange={(e)=>setSearch(e.target.value)} className='border w-[40%] h-12 p-3 ' placeholder='User search by Name'/>
       {isLoading && <h1 className="text-center text-lg font-medium">...Loading</h1>}
       {isError && <h1 className="text-center text-lg font-medium text-red-600">Something went wrong</h1>}
 
@@ -39,7 +40,26 @@ const User = () => {
             </thead>
 
             <tbody>
-              {[...users].reverse().map((user) => (
+              {/* {[...users].reverse().map((user) => (
+                <tr key={user.id} className="hover:bg-gray-100 border">
+                  <td className="border px-4 py-2">{user.name}</td>
+                  <td className=" px-4 py-2">{user.lastname}</td>
+                  <td className="border px-4 py-2">{user.age}</td>
+                  <td className="border px-4 py-2">{user.email}</td>
+                  <td className=" px-4 py-2 space-x-2 flex flex-wrap gap-2">
+                    <Link to={`/edit/${user.id}`}>
+                      <button className='bg-green-600 hover:bg-green-700 text-white py-1 px-3 rounded-md transition duration-200'>Edit</button>
+                    </Link>
+                    <button
+                      className='bg-red-600 hover:bg-red-700 text-white py-1 px-3 rounded-md transition duration-200'
+                      onClick={() => deleteUser(user.id)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))} */}
+              {[...filteredUsers].reverse().map((user) => (
                 <tr key={user.id} className="hover:bg-gray-100 border">
                   <td className="border px-4 py-2">{user.name}</td>
                   <td className=" px-4 py-2">{user.lastname}</td>
