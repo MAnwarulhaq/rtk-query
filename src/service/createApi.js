d005be7cc201b6a713f4dee949444f5d144399ea
MAnwarulhaq <muhammadanwarulhaq15@gmail.com>
2025-11-26 14:36:40 +0500

    Update the code

--- src/service/createApi.js
+++ src/service/createApi.js
@@ -1,10 +1,31 @@
-import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+// import { createApi,fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
+
+// export const api = createApi({
+//     reducerPath:"api",
+//     baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000"}),
+//     endpoints: (builder)=>({
+
+//     })
+// })
+
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    reducerPath:"api",
-    baseQuery: fetchBaseQuery({baseUrl: "http://localhost:3000"}),
-    endpoints: (builder)=>({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3000",
 
-    })
-})
+    prepareHeaders: (headers, { getState }) => {
+      const token = localStorage.getItem("token");   // token get karna
+
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);  // header set karna
+      }
+
+      return headers;
+    },
+  }),
+
+  endpoints: (builder) => ({}),
+});
